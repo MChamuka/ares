921a61c016f2637df1f6451019ad33aa06cd05a9
MChamuka <maddewithanagechamuka@gmail.com>
2025-03-27 22:45:46 +0530

    backend setup

---
+++ backend/target/debug/deps/backend-342ecd7239289a35.d
@@ -0,0 +1,5 @@
+C:\Users\HP\Desktop\ares\backend\target\debug\deps\libbackend-342ecd7239289a35.rmeta: src\main.rs
+
+C:\Users\HP\Desktop\ares\backend\target\debug\deps\backend-342ecd7239289a35.d: src\main.rs
+
+src\main.rs:
---
+++ backend/target/debug/deps/backend-73b83fd0778bdada.d
@@ -0,0 +1,5 @@
+C:\Users\HP\Desktop\ares\backend\target\debug\deps\libbackend-73b83fd0778bdada.rmeta: src\main.rs
+
+C:\Users\HP\Desktop\ares\backend\target\debug\deps\backend-73b83fd0778bdada.d: src\main.rs
+
+src\main.rs:
